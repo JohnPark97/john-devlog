3291ccc56f5ccd1d2df885772143ec918f85ea6e
John Park <h.johnpark97@gmail.com>
2023-09-17 19:06:03 -0700

    make title page better

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,11 @@
+import MyGifComponent from "./gifs/becca_dancing";
+
 function Header() {
   return (
     <header>
-      <h1>John Doe</h1>
+      <h1>John Park</h1>
       <p>Software Developer</p>
+      <MyGifComponent />
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,34 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import WorksSection from '../components/WorkSection';
+// pages/index.tsx
+import React from 'react';
+import Head from 'next/head';
+import MyGifComponent from '@components/gifs/becca_dancing';
 
-export default function Home() {
+const Home: React.FC = () => {
+  const title = "John Park";
+
   return (
-    <div>
-      <Header />
-      <main>
-        <section>
-          <h2>My Vision</h2>
-          <p>
-            Describe your vision as a software developer here...
-          </p>
-        </section>
-        <WorksSection />
+    <div className="container">
+      <Head>
+        <title>Your Professional Site</title>
+      </Head>
+
+      <header className="header">
+        <h1 className="title">
+          {title.split("").map((char, index) => 
+            <span key={index} style={{ animationDelay: `${index * 0.1}s` }}>{char}</span>
+          )}
+        </h1>
+      </header>
+
+      <main className="main">
+        <MyGifComponent />
+        <p className="description">
+          A software developer specializing in modern web and mobile applications.
+        </p>
+        <button className="button">Learn More</button>
       </main>
-      <Footer />
     </div>
   );
 }
+
+export default Home;
