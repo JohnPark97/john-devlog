9940ba9570786c2236deda704f9f53490812c96a
John Park <john.park@clio.com>
2023-12-04 17:55:18 -0800

    Add two more cats!

--- components/CatContainer.tsx
+++ components/CatContainer.tsx
@@ -21,7 +21,7 @@ const CatContainer: React.FC = () => {
       const waggingCat = await renderCatSprite({
         animationJson: 'spritesheets/cat/waggingCat.json', 
         animationName: 'catWagging', 
-        animationSpeed: 0.1, 
+        animationSpeed: 0.2, 
         position: {x: 0, y: 0}, 
         scale: {x: 3, y: 3}
       });
@@ -38,11 +38,27 @@ const CatContainer: React.FC = () => {
         animationJson: 'spritesheets/cat/heartCats.json', 
         animationName: 'heart', 
         animationSpeed: 0.1, 
+        position: {x: 400, y: 100}, 
+        scale: {x: 3, y: 3}
+      });
+
+      const pullupCat = await renderCatSprite({
+        animationJson: 'spritesheets/cat/pullupCat.json', 
+        animationName: 'pullup', 
+        animationSpeed: 0.2, 
+        position: {x: 500, y: 100}, 
+        scale: {x: 3, y: 3}
+      });
+
+      const drawingCat = await renderCatSprite({
+        animationJson: 'spritesheets/cat/drawingCat.json', 
+        animationName: 'draw', 
+        animationSpeed: 0.2, 
         position: {x: 300, y: 100}, 
         scale: {x: 3, y: 3}
       });
 
-      app.stage.addChild(waggingCat, angryCat, heartCats);
+      app.stage.addChild(waggingCat, angryCat, heartCats, pullupCat, drawingCat);
       app.start();
     };
 
